e367e5c563545f227164beabf6508fb0d88c8085
lmoranchel <l.moranchel@hotmail.com>
2024-09-18 05:23:31 -0700

    add static params

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -3,6 +3,13 @@ import Link from 'next/link'
 import {getPostData, getSortedPostsData} from '@/lib/posts'
 import getFormattedDate from '@/lib/getFormattedDate'
 
+export function generateStaticParams() {
+   const posts = getSortedPostsData()
+
+   return posts.map(post => ({
+      postId: post.id
+   }))
+}
 
 export function generateMetadata({params}: { params: { postId: string } }) {
    const posts = getSortedPostsData()
