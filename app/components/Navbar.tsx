0136ebfe324adc1004a6870878dde648dfae2bd2
lmoranchel <l.moranchel@hotmail.com>
2024-09-18 03:53:37 -0700

    add icons

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,29 @@
 import Link from 'next/link'
+import { FaYoutube, FaTwitter, FaGithub, FaLaptop } from 'react-icons/fa'
 
 const Navbar = () => {
    return (
       <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
          <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
-            <Link href="/" className="text-white/90 no-underline hover:text-white">
-               Leopoldo Moranchel
-            </Link>
+            <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+               <Link href="/" className="text-white/90 no-underline hover:text-white">
+                  Leopoldo Moranchel
+               </Link>
+            </h1>
+            <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
+               <Link className="text-white/90 hover:text-white" href="https://www.youtube.com/">
+                  <FaYoutube/>
+               </Link>
+               <Link className="text-white/90 hover:text-white" href="https://github.com/lmoranchel">
+                  <FaLaptop/>
+               </Link>
+               <Link className="text-white/90 hover:text-white" href="https://github.com/lmoranchel">
+                  <FaGithub/>
+               </Link>
+               <Link className="text-white/90 hover:text-white" href="https://twitter.com/lmorancheldev">
+                  <FaTwitter/>
+               </Link>
+            </div>
          </div>
       </nav>
    );
