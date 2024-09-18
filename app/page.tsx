533b9bc28d696fb34e5df404f984e8f58d596998
lmoranchel <l.moranchel@hotmail.com>
2024-09-18 03:26:48 -0700

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 export default function Home() {
   return (
-    <main >
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋🏻&nbsp;
+        <span className="whitespace-nowrap">
+          I'm <span className="font-bold">Leopoldo</span>.
+        </span>
+      </p>
 
     </main>
   )
