bd8cc241345df1cd84202335522a23bf554c4c32
lmoranchel <l.moranchel@hotmail.com>
2024-09-27 21:38:24 -0700

    add video component

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -2,6 +2,7 @@ import {compileMDX} from 'next-mdx-remote/rsc'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeHighlight from 'rehype-highlight'
 import rehypeSlug from 'rehype-slug'
+import Video from '@/app/components/Video'
 
 type Filetree = {
    "tree": [
@@ -32,6 +33,9 @@ export async function getPostByName(fileName: string): Promise<BlogPost | undefi
 
    const {frontmatter, content} = await compileMDX<{ title: string, date: string, tags: string[] }>({
       source: rawMDX,
+      components: {
+         Video,
+      },
       options: {
          parseFrontmatter: true,
          mdxOptions: {
